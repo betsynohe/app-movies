62463815593046cffb1bd7608dcdf02ac6bd995b
betsynohe <betsyguitian49@gmail.com>
2024-01-21 02:10:32 -0300

    navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,13 @@
 import { Menubar } from "primereact/menubar";
+import { InputText } from "primereact/inputtext";
+import Logo from "../assets/pnglogo.png";
 
 export default function NavBar() {
     const items = [
         {
-            label: "Home",
-            icon: "pi pi-home",
-        },
-        {
             label: "Ultimos Lanzamientos",
             icon: "pi pi-envelope",
+            className: "text-white",
         },
         {
             label: "Populares",
@@ -18,15 +17,27 @@ export default function NavBar() {
             label: "Favoritos",
             icon: "pi pi-star",
         },
-        {
-            label: "Buscar",
-            icon: "pi pi-search",
-        },
     ];
+    const start = (
+        <img alt="logo" src={Logo} height="40" className="mr-2"></img>
+    );
+    const end = (
+        <div className="flex align-items-center gap-2">
+            <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText placeholder="Buscar" />
+            </span>
+        </div>
+    );
 
     return (
-        <div className="card">
-            <Menubar model={items} />
+        <div className="card surface-500 hover:surface-ground">
+            <Menubar
+                className="surface-ground hover:surface-500  font-bold m-2 flex align-items-center justify-content-center"
+                model={items}
+                start={start}
+                end={end}
+            />
         </div>
     );
 }
