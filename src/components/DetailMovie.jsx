bc2e64198945e831f084f63aa44d4ba341d4e479
betsynohe <betsyguitian49@gmail.com>
2024-02-01 01:27:32 -0300

    add component and function the spinner

--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -6,6 +6,8 @@ import NotImagenPoster from "../assets/not-image-poster.png";
 import NotImageBackdrop from "../assets/not-image-backdrop.jpg";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { FavoriteContext } from "../context/FavoriteContext";
+import  {useMovieLoading }from "../customHooks/useMovieLoading";
+import SpinnerMovie from "./SpinnerMovie";
 
 export default function DetailMovie() {
     const apiKey = import.meta.env.VITE_TMDB_API_KEY;
@@ -13,6 +15,8 @@ export default function DetailMovie() {
     const { getData, data } = useMovies();
     const { getFavorite, addFavorite, removeFavorite } =
         useContext(FavoriteContext);
+    const loading = useMovieLoading();
+
     useEffect(() => {
         getData(
             `https://api.themoviedb.org/3/movie/${id}?language=es-ES&api_key=${apiKey}`
@@ -21,122 +25,132 @@ export default function DetailMovie() {
 
     return (
         <div>
-            <div
-                style={{
-                    height: "100vh",
-                    backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), 
+            {loading ? (
+                <SpinnerMovie />
+            ) : (
+                <div
+                    style={{
+                        height: "100vh",
+                        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), 
             rgba(255, 255, 255, 0.4)), url( ${
                 data.backdrop_path
                     ? `https://image.tmdb.org/t/p/original${data.backdrop_path}?api_key=${apiKey}`
                     : NotImageBackdrop
             })`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    padding: "20px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}>
-                <Container>
-                    <Row>
-                        <Col xs={12} md={6} lg={4}>
-                            <Image
-                                src={
-                                    data.poster_path
-                                        ? `https://image.tmdb.org/t/p/w300${data.poster_path}?api_key=${apiKey}`
-                                        : NotImagenPoster
-                                }
-                                alt=""
-                                fluid
-                                style={{
-                                    maxWidth: "100%",
-                                    borderRadius: "10px",
-                                    margin: "0 auto",
-                                }}
-                            />
-                        </Col>
-                        <Col xs={12} md={6} lg={8}>
-                            <Card
-                                style={{
-                                    width: "100%",
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    height: "100%",
-                                    opacity: 0.8,
-                                }}>
-                                <Card.Body>
-                                    <Card.Title
-                                        style={{
-                                            fontWeight: "900",
-                                            fontSize: "32px",
-                                            display: "flex",
-                                            justifyContent:"space-evenly",
-                                            marginBottom: "10px",
-                                        }}>
-                                        <div>
-                                            {data.title} -{" "}
-                                            <span className="font-normal">
-                                                {
-                                                    data.release_date?.split(
-                                                        "-"
-                                                    )[0]
-                                                }
-                                            </span>
-                                        </div>
-                                        <div
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        padding: "20px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={6} lg={4}>
+                                <Image
+                                    src={
+                                        data.poster_path
+                                            ? `https://image.tmdb.org/t/p/w300${data.poster_path}?api_key=${apiKey}`
+                                            : NotImagenPoster
+                                    }
+                                    alt=""
+                                    fluid
+                                    style={{
+                                        maxWidth: "100%",
+                                        borderRadius: "10px",
+                                        margin: "0 auto",
+                                    }}
+                                />
+                            </Col>
+                            <Col xs={12} md={6} lg={8}>
+                                <Card
+                                    style={{
+                                        width: "100%",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        height: "100%",
+                                        opacity: 0.8,
+                                    }}>
+                                    <Card.Body>
+                                        <Card.Title
                                             style={{
-                                                fontSize: "24px",
+                                                fontWeight: "900",
+                                                fontSize: "32px",
+                                                display: "flex",
+                                                justifyContent: "space-evenly",
+                                                marginBottom: "10px",
                                             }}>
                                             <div>
-                                                {getFavorite(data.id) ? (
-                                                    <FaStar
-                                                        onClick={() =>
-                                                            removeFavorite(data)
-                                                        }
-                                                        style={{
-                                                            cursor: "pointer",
-                                                        }}
-                                                    />
-                                                ) : (
-                                                    <FaRegStar
-                                                        onClick={() =>
-                                                            addFavorite(data)
-                                                        }
-                                                        style={{
-                                                            cursor: "pointer",
-                                                        }}
-                                                    />
-                                                )}
+                                                {data.title} -{" "}
+                                                <span className="font-normal">
+                                                    {
+                                                        data.release_date?.split(
+                                                            "-"
+                                                        )[0]
+                                                    }
+                                                </span>
                                             </div>
-                                        </div>
-                                    </Card.Title>
-                                    <Card.Subtitle
-                                        className="mb-2 mt-4 fs-4"
-                                        style={{ fontWeight: "600" }}>
-                                        Descripción:
-                                    </Card.Subtitle>
-                                    <Card.Text>{data.overview}</Card.Text>
-                                    <Card.Subtitle
-                                        className="mb-2 mt-4 fs-4"
-                                        style={{ fontWeight: "600" }}>
-                                        Géneros:
-                                    </Card.Subtitle>
-                                    <ListGroup horizontal>
-                                        {data.genres &&
-                                            data.genres.map(({ name, id }) => (
-                                                <ListGroup.Item
-                                                    variant="dark"
-                                                    key={id}>
-                                                    {name}
-                                                </ListGroup.Item>
-                                            ))}
-                                    </ListGroup>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                                            <div
+                                                style={{
+                                                    fontSize: "24px",
+                                                }}>
+                                                <div>
+                                                    {getFavorite(data.id) ? (
+                                                        <FaStar
+                                                            onClick={() =>
+                                                                removeFavorite(
+                                                                    data
+                                                                )
+                                                            }
+                                                            style={{
+                                                                cursor: "pointer",
+                                                            }}
+                                                        />
+                                                    ) : (
+                                                        <FaRegStar
+                                                            onClick={() =>
+                                                                addFavorite(
+                                                                    data
+                                                                )
+                                                            }
+                                                            style={{
+                                                                cursor: "pointer",
+                                                            }}
+                                                        />
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </Card.Title>
+                                        <Card.Subtitle
+                                            className="mb-2 mt-4 fs-4"
+                                            style={{ fontWeight: "600" }}>
+                                            Descripción:
+                                        </Card.Subtitle>
+                                        <Card.Text>{data.overview}</Card.Text>
+                                        <Card.Subtitle
+                                            className="mb-2 mt-4 fs-4"
+                                            style={{ fontWeight: "600" }}>
+                                            Géneros:
+                                        </Card.Subtitle>
+                                        <ListGroup horizontal>
+                                            {data.genres &&
+                                                data.genres.map(
+                                                    ({ name, id }) => (
+                                                        <ListGroup.Item
+                                                            variant="dark"
+                                                            key={id}>
+                                                            {name}
+                                                        </ListGroup.Item>
+                                                    )
+                                                )}
+                                        </ListGroup>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            )}
         </div>
     );
 }
